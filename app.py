7e3659241a8ed833096e283ee838c05da8e6edc4
wx <hwasin001@163.com>
2017-05-07 20:28:31 +0800

    add orm.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import logging;logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
 from datetime import datetime
@@ -6,15 +6,26 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body = b'<h1>Awesome</h1>')
+
+
+# 异步函数，使用async/await
 
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    app = web.Application(loop = loop)
+    app.router.add_route('GET', '/',index)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
-loop = asyncio.get_event_loop()
+# 获得 eventloop引用，将init函数（协程）放到eventloop中执行，这里只有一个协程
+# event 负责 I/O 事件通知而 loop 负责循环处理 I/O 通知并在就绪时调用回调
+
+loop=asyncio.get_event_loop()
+
+# 协程自动封装成 task，放入eventloop中执行。
+
 loop.run_until_complete(init(loop))
+
+# 一直运行
 loop.run_forever()
